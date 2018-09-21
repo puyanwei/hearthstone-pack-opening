e5e1db10628ed54277bf940e033b2c3442c69471
Puyan Wei <puyan.wei@mountainwarehouse.com>
2018-09-21 15:57:38 +0100

    Added axios, added template get code for api@

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <p>Hearthstone Packs</p>
+                <h1>Hearthstone Packs</h1>
+                <p>PANEL COMPONENT</p>
+                <button>Open Packs!</button>
             </div>
         );
     }
+
+    componentDidMount() {
+        this.getAPIData();
+    }
+
+    getAPIData = () => {
+        const url = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards';
+
+        axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 }
 
 export default App;
